74c611b8659e30210c68e2deba0a14f011b4c924
Paul Borowicz <pborowicz@users.noreply.github.com>
2019-03-21 11:03:40 -0400

    updates

---
+++ test_using_ssh_batch_mode.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+ssh -oBatchMode=yes -oConnectTimeout=3 -l 	paul.borowicz 	10.18.3.49 	 hostname
+ssh -oBatchMode=yes -oConnectTimeout=3 -l 	paul.borowicz 	10.18.2.48 	 hostname
+ssh -oBatchMode=yes -oConnectTimeout=3 -l 	paul.borowicz 	10.18.2.37 	 hostname
+ssh -oBatchMode=yes -oConnectTimeout=3 -l 	paul.borowicz 	10.18.2.123 	 hostname
+ssh -oBatchMode=yes -oConnectTimeout=3 -l 	paul.borowicz 	10.18.3.13 	 hostname
+ssh -oBatchMode=yes -oConnectTimeout=3 -l 	paul.borowicz 	10.18.1.4 	 hostname
+ssh -oBatchMode=yes -oConnectTimeout=3 -l 	paul.borowicz 	10.18.3.25 	 hostname
+ssh -oBatchMode=yes -oConnectTimeout=3 -l 	paul.borowicz 	10.18.1.28 	 hostname
+ssh -oBatchMode=yes -oConnectTimeout=3 -l 	paul.borowicz 	10.18.3.26 	 hostname
+ssh -oBatchMode=yes -oConnectTimeout=3 -l 	paul.borowicz 	10.18.3.12 	 hostname
+ssh -oBatchMode=yes -oConnectTimeout=3 -l 	paul.borowicz 	10.18.3.61 	 hostname
+ssh -oBatchMode=yes -oConnectTimeout=3 -l 	paul.borowicz 	10.18.2.15 	 hostname
+ssh -oBatchMode=yes -oConnectTimeout=3 -l 	paul.borowicz 	10.18.2.172 	 hostname
+ssh -oBatchMode=yes -oConnectTimeout=3 -l 	paul.borowicz 	10.18.2.60 	 hostname
+ssh -oBatchMode=yes -oConnectTimeout=3 -l 	paul.borowicz 	10.18.2.107 	 hostname
+ssh -oBatchMode=yes -oConnectTimeout=3 -l 	paul.borowicz 	10.18.3.28 	 hostname
+ssh -oBatchMode=yes -oConnectTimeout=3 -l 	paul.borowicz 	10.18.2.114 	 hostname
+ssh -oBatchMode=yes -oConnectTimeout=3 -l 	paul.borowicz 	10.18.2.81 	 hostname
+ssh -oBatchMode=yes -oConnectTimeout=3 -l 	paul.borowicz 	10.18.3.21 	 hostname
+ssh -oBatchMode=yes -oConnectTimeout=3 -l 	paul.borowicz 	10.18.2.51 	 hostname
+ssh -oBatchMode=yes -oConnectTimeout=3 -l 	paul.borowicz 	10.18.3.14 	 hostname
+ssh -oBatchMode=yes -oConnectTimeout=3 -l 	paul.borowicz 	10.18.3.6 	 hostname
+ssh -oBatchMode=yes -oConnectTimeout=3 -l 	paul.borowicz 	10.18.2.189 	 hostname
+ssh -oBatchMode=yes -oConnectTimeout=3 -l 	paul.borowicz 	10.18.2.122 	 hostname
+ssh -oBatchMode=yes -oConnectTimeout=3 -l 	paul.borowicz 	10.18.2.180 	 hostname
+ssh -oBatchMode=yes -oConnectTimeout=3 -l 	paul.borowicz 	10.18.3.10 	 hostname
+ssh -oBatchMode=yes -oConnectTimeout=3 -l 	paul.borowicz 	10.18.3.54 	 hostname
+ssh -oBatchMode=yes -oConnectTimeout=3 -l 	paul.borowicz 	10.18.3.8 	 hostname
+ssh -oBatchMode=yes -oConnectTimeout=3 -l 	paul.borowicz 	10.18.3.53 	 hostname
+ssh -oBatchMode=yes -oConnectTimeout=3 -l 	paul.borowicz 	10.18.2.183 	 hostname
+ssh -oBatchMode=yes -oConnectTimeout=3 -l 	paul.borowicz 	10.18.2.78 	 hostname
+ssh -oBatchMode=yes -oConnectTimeout=3 -l 	paul.borowicz 	10.18.2.182 	 hostname
+ssh -oBatchMode=yes -oConnectTimeout=3 -l 	paul.borowicz 	10.18.2.79 	 hostname
+ssh -oBatchMode=yes -oConnectTimeout=3 -l 	paul.borowicz 	10.18.3.39 	 hostname
+ssh -oBatchMode=yes -oConnectTimeout=3 -l 	paul.borowicz 	10.18.3.46 	 hostname
+ssh -oBatchMode=yes -oConnectTimeout=3 -l 	paul.borowicz 	10.18.2.155 	 hostname
+ssh -oBatchMode=yes -oConnectTimeout=3 -l 	paul.borowicz 	10.18.3.24 	 hostname
+ssh -oBatchMode=yes -oConnectTimeout=3 -l 	paul.borowicz 	10.18.3.38 	 hostname
+ssh -oBatchMode=yes -oConnectTimeout=3 -l 	paul.borowicz 	10.18.3.7 	 hostname
+ssh -oBatchMode=yes -oConnectTimeout=3 -l 	paul.borowicz 	10.18.0.73 	 hostname
+ssh -oBatchMode=yes -oConnectTimeout=3 -l 	paul.borowicz 	10.18.2.167 	 hostname
+ssh -oBatchMode=yes -oConnectTimeout=3 -l 	paul.borowicz 	10.18.2.17	 hostname
+
